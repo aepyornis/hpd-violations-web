a206f05d640150e78d8bedc22738871c005ed1a1
ziggy <ziggy@elephant-bird.net>
2016-05-17 17:44:47 -0400

    Add NavBar Component

---
+++ src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {navbar} from '../style';
+
+
+const NavBar = () => (
+  <div style={navbar.container}>
+    <div style={navbar.infoMenuButton}>
+      <a href="#">
+        <img style={navbar.icon} src="open-iconic-master/svg/menu.svg" alt="menu"></img>
+      </a>
+    </div>
+    <a href="#" style={navbar.text}>HPD Violations</a>
+    
+  </div>
+)
+
+export default NavBar;
---
+++ src/style.js
@@ -0,0 +1,26 @@
+export const navbar = {};
+
+navbar.container = {
+  display: 'block',
+  width: '100%',
+  minHeight: '50px',
+  lineHeight: '50px',
+  backgroundColor: '#b3e2cd'
+};
+
+navbar.infoMenuButton = {
+  display: 'inline'
+};
+
+navbar.icon = {
+  marginLeft: '10px',
+  maringRight: '30x',
+  width: '20px',
+  height: '20px'
+};
+
+navbar.text = {
+  color: 'black',
+  fontSize: '18px',
+  paddingLeft: '20px'
+};
