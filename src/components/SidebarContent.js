55b885acb865c578b0ae3083702d26dfdb644e53
ziggy <ziggy@elephant-bird.net>
2016-05-31 12:20:48 -0400

    Update sidebar text

--- src/components/SidebarContent.js
+++ src/components/SidebarContent.js
@@ -12,17 +12,17 @@ export const SidebarContent = ({dispatch}) =>(
     <div className="tr pt1 pr2"><button onClick={()=>dispatch(closeSidebar())}>Close</button></div>
 
     <div className="tc pt1"><span className="tc f4">Housing Violations in NYC</span></div>
-    <p>The Department of Housing, Preservation, and Development (HPD) issues violation to owners if their buildings fail to conform to the NYC Housing Maintenance Code or the NYS Multiple Dwelling Law. This amounts to a lot of violations: there are nearly <strong>2 million</strong> open housing violations and HPD had issued over million violations since 2015. Landlords regularly fail to maintain their building and many thousands of tenants are forced to live in sub-par buildings with  uninhabitable conditions. </p>
+    <p>The Department of Housing, Preservation, and Development (HPD) issues violations to owners if their buildings do not conform to the NYC <a href="http://tenant.net/Other_Laws/HMC/index.html">Housing Maintenance Code</a> or the <a href="http://tenant.net/Other_Laws/MDL/mdltoc.html">NYS Multiple Dwelling Law</a>. This amounts to a lot of violations: there are nearly <strong>2 million</strong> open housing violations and HPD has issued over a million violations since 2015. Landlords regularly fail to maintain their buildings and, consequently,  many thousands of tenants are forced to live in sub-par buildings with  uninhabitable conditions. </p>
     <p>There are 4 classes of violations:</p>
 
-    <p><strong>C - "Immediatly Hazardous"</strong> These include lack of heat/hot water, lead-paint, and other extreme conditions. An owner has generally only 24 hours to fix these violations.<br /><br /> <strong>I -</strong> This type of violation typically indicates that the owner failed to register their building properly with HPD.<br /><br /><strong>B - "Hazardous"</strong> An owner has 30 days to correct this type of violation.<br /><br /> <strong>A - "Non-hazardous".</strong> This includes less issues such as minor leaks. An owner has 90 days to correct this type of violation.</p>
+    <p><strong>C - "Immediatly Hazardous"</strong> These include lack of heat/hot water, lead-paint, and other extreme conditions. An owner has generally only 24 hours to fix these violations.<br /><br /> <strong>I -</strong> This type of violation typically indicates that the owner failed to register their building properly with HPD.<br /><br /><strong>B - "Hazardous"</strong> An owner has 30 days to correct this type of violation.<br /><br /> <strong>A - "Non-hazardous".</strong> These are relatively minor issues such as small leaks. An owner has 90 days to correct this type of violation.</p>
 
     <p><strong>About this data:</strong><br />The data for this websites comes from <a href="http://www1.nyc.gov/site/hpd/about/open-data.page">HPD's open data</a> portal. It contains both open and closed violations since January 2015 and all open violations even if the violation was issued years ago.
 
     HPD does provide a service, <a href="http://www1.nyc.gov/site/hpd/about/hpdonline.page">HPD ONLINE</a>, where you can look up violations and other information.</p>
 
 
-    <p><strong>What to do if you building has violations:</strong><br />
+    <p><strong>What to do if your building has violations:</strong><br />
 
     If your landlord won't fix them, you can take your landlord to housing court to force them to fix the problems and even potentially receive a rent abatement.</p>
 
